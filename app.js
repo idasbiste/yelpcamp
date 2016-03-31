10b74eea9db63124c258b9c08e18e53d629212b9
idasbiste <idasbistefernandes@gmail.com>
2016-03-31 18:18:21 +0000

    Add node & npm versions to package.json

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function () {
-    console.log("The YelpCamp Server is started.");
+    console.log("YelpCamp has started...");
 });
